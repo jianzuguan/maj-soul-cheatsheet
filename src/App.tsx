fe13b1b8649213e4e5694287280b1c47a29daabd
Jianzu Guan <jianzuguan@luminalearning.com>
2021-11-26 17:45:04 +0000

    add Yaku component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,16 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import yakuList from "./data/yaku.json";
-import { MahJongTile } from "./Components/MahJongTile";
-import { YakuGroupSpacing } from "./Components/YakuGroupSpacing";
-import { getPatternArr } from "./utils/getPatternArr";
+import { Yaku } from "./Components/Yaku";
 
 function App() {
   return (
     <div className="container flex flex-col justify-center items-center">
       <img src={logo} className="App-logo" alt="logo" />
 
-      {yakuList.map((yaku, index) => {
-        const patternCodes = getPatternArr(yaku.pattern);
-        return (
-          <div
-            key={`${yaku.name}`}
-            className={[
-              "w-full flex flex-col",
-              "mb-1",
-              "p-4",
-              "border-2 border-indigo-900",
-            ].join(" ")}
-          >
-            <div className="flex flex-row justify-between">
-              <h2 className="text-yellow-300">{yaku.name}</h2>
-              {yaku.closedOnly && <p className="text-green-500">门前清限定</p>}
-              {yaku.openReduceHan && (
-                <p className="text-green-500">副露减1番</p>
-              )}
-              {yaku.dealerOnly && <p className="text-green-500">庄家限定</p>}
-              {yaku.nonDealerOnly && <p className="text-green-500">子家限定</p>}
-            </div>
-            <p className="text-gray-400">{yaku.description}</p>
-            <div className="w-full flex flex-row">
-              {patternCodes.map((patternCode, patternCodeIndex: number) =>
-                patternCode ? (
-                  <MahJongTile
-                    key={`${yaku.name}${patternCodeIndex}`}
-                    code={patternCode}
-                  />
-                ) : (
-                  <YakuGroupSpacing key={`${yaku.name}${patternCodeIndex}`} />
-                )
-              )}
-            </div>
-          </div>
-        );
-      })}
+      {yakuList.map((yaku, index) => (
+        <Yaku yaku={yaku} key={index} />
+      ))}
     </div>
   );
 }
---
+++ src/Components/Yaku.tsx
@@ -0,0 +1,63 @@
+import { getPatternArr } from "../utils/getPatternArr";
+import { MahJongTile } from "./MahJongTile";
+import { YakuGroupSpacing } from "./YakuGroupSpacing";
+
+interface Props {
+  yaku: {
+    name: string;
+    description: string;
+    pattern: string;
+    hanValue: string;
+    closedOnly?: boolean;
+    openReduceHan?: boolean;
+    dealerOnly?: boolean;
+    nonDealerOnly?: boolean;
+  };
+}
+export function Yaku(props: Props) {
+  const { yaku } = props;
+  const {
+    name,
+    description,
+    pattern,
+    hanValue,
+    closedOnly,
+    openReduceHan,
+    dealerOnly,
+    nonDealerOnly,
+  } = yaku;
+
+  const patternCodes = getPatternArr(pattern);
+  return (
+    <div
+      key={`${name}`}
+      className={[
+        "w-full flex flex-col",
+        "mb-1",
+        "p-4",
+        "border-2 border-indigo-900",
+      ].join(" ")}
+    >
+      <div className="flex flex-row justify-between">
+        <h2 className="text-yellow-300">{name}</h2>
+        {closedOnly && <p className="text-green-500">门前清限定</p>}
+        {openReduceHan && <p className="text-green-500">副露减1番</p>}
+        {dealerOnly && <p className="text-green-500">庄家限定</p>}
+        {nonDealerOnly && <p className="text-green-500">子家限定</p>}
+      </div>
+      <p className="text-gray-400">{description}</p>
+      <div className="w-full flex flex-row">
+        {patternCodes.map((patternCode, patternCodeIndex: number) =>
+          patternCode ? (
+            <MahJongTile
+              key={`${name}${patternCodeIndex}`}
+              code={patternCode}
+            />
+          ) : (
+            <YakuGroupSpacing key={`${name}${patternCodeIndex}`} />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
