5ede2491a7c006c1931aefb689ace82da8aac606
Jianzu Guan <jianzuguan@luminalearning.com>
2021-11-26 00:07:44 +0000

    display yaku in ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import yakuList from "./data/yaku.json";
+import { MahJongTile } from "./Components/MahJongTile";
+import { YakuGroupSpacing } from "./Components/YakuGroupSpacing";
+import { getPatternArr } from "./utils/getPatternArr";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container flex flex-col justify-center items-center">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      {yakuList.map((yaku, index) => {
+        const patternCodes = getPatternArr(yaku.pattern);
+        return (
+          <div
+            key={`${yaku.name}`}
+            className={["w-full flex flex-col", "mb-1", "border-2"].join(" ")}
+          >
+            <div className="flex flex-row justify-between">
+              <h2>{yaku.name}</h2>
+              {yaku.closedHandOnly && <p>门前清限定</p>}
+            </div>
+            <p>{yaku.description}</p>
+            <div className="w-full flex flex-row">
+              {patternCodes.map((patternCode, patternCodeIndex: number) =>
+                patternCode ? (
+                  <MahJongTile
+                    key={`${yaku.name}${patternCodeIndex}`}
+                    code={patternCode}
+                  />
+                ) : (
+                  <YakuGroupSpacing key={`${yaku.name}${patternCodeIndex}`} />
+                )
+              )}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
